8579a9b456caffc5e37fb80d403878375b379f02
aalonsocam <aalonoscam@gmail.com>
2015-10-01 13:41:43 +0200

    -

---
+++ app/src/main/java/es/aac/listadelacompra/entidades/ListaCompraObserver.java
@@ -0,0 +1,8 @@
+package es.aac.listadelacompra.entidades;
+
+/**
+ * Created by manana on 01/10/15.
+ */
+public interface ListaCompraObserver {
+    void OnChanged(GrupoProductos grupoProductos);
+}
